a71c755918bd85160087ab4ce2a58eda5ed01290
Mth71 <mthrocha07@outlook.com>
2026-05-04 09:02:32 -0300

    add initial screen scroll

--- main.c
+++ main.c
@@ -7,8 +7,12 @@ int main(){
     SetTargetFPS(60);
 
     Rectangle carro = {370, 500, 60,100};
-
+    float offset = 0;
     while(!WindowShouldClose()){
+        offset += 5.0f;
+        if(offset >= 100){
+            offset -0;
+        }
         BeginDrawing();
         ClearBackground(DARKGRAY);
         
@@ -26,7 +30,15 @@ int main(){
         }
 
         DrawRectangleRec(carro, RED);
-
+        
+        //tela com scroll
+        for(int i =0; i<6;i++){
+            int y = (int)(offset+i*100)%600;
+            DrawRectangle(390, y, 10,60,YELLOW);
+        } 
+        // bordas da pista
+        DrawRectangle(40,0,15,750, WHITE); // borda da esquerda
+        DrawRectangle(745,0,15,750, WHITE); // borda direita
         EndDrawing();
     }
     CloseWindow();
